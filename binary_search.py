cd96cff69ea101233d2f10a52e224355a4aac329
oh980225 <oh980225@gmail.com>
2020-01-31 13:07:12 +0000

    Add binary_search

---
+++ binary_search.py
@@ -0,0 +1,12 @@
+def binary_search(a, x):
+  start = 0
+  end = len(a) - 1
+  while start <= end:
+    mid = (start + end) // 2
+    if x == a[mid]:
+      return mid
+    elif x > a[mid]:
+      start = mid + 1
+    else:
+      end = mid - 1
+  return -1
